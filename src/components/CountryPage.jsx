5d15c5ad02cd17a6d2732d0162430bf29d1b8a1b
jmfreitas <jmfreitas@ua.pt>
2019-10-24 12:43:06 +0100

    commit images fixed

--- src/components/CountryPage.jsx
+++ src/components/CountryPage.jsx
@@ -9,19 +9,15 @@ function CountryPage(props){
     const [arrayImages, setArrayImages] = useState([]);
     const [portalOpen, setPortalOpen] = useState(false);
     const [portalImage, setPortalImage] = useState('');
+    const [countryName, setCountryName] = useState('');
 
     useEffect(() => {
         fetchApi();
     }, []);
 
-    useEffect(() => {
-        console.log('PORTAL', portalOpen)
-    })
-
-
     const fetchApi = async() => {
         await getByCountryName(props.match.params.name).then(response => {
-            console.log(response)
+            setCountryName(response.data[0].name);
         }).then(async () => {
             await getImagesByCountry(props.match.params.name).then(response => {
                 setArrayImages(response);
@@ -42,33 +38,46 @@ function CountryPage(props){
 
 
     return(
-        <div className='content'>
-            <div className='container'>
-                <div className='country_images'>
-                    <h2 className="no-span">Images</h2>
-                    <div className='row'>
-                        <div className='column'>
-                            {arrayImages.map((data, index) => {
-                                if(isEven(index) || index === 0) {
-                                    return <img className='images' onClick={() => {handleImage(data)}} alt='' src={data}/>
-                                }
-                            })}
-                        </div>
-                        <div className='column'>
-                            {arrayImages.map((data, index) => {
-                                if(isOdd(index)) {
-                                    return <img className='images' onClick={() => {handleImage(data)}} alt='' src={data}/>
-                                }
-                            })}
-                        </div>
+        <div>
+            {countryName !== '' &&
+            <div className='content'>
+                <div className='container'>
+                    <div className='country_nfo'>
+                        <h2 className="no-span">About {countryName}</h2>
                     </div>
-                    {portalOpen &&
-                    <Portal id='modal-root'>
-                        <BuildModal openPortal={openPortal} image={portalImage}/>
-                    </Portal>
-                    }
+                </div>
+                <div className='container'>
+                    <div className='country_info'>
+                        <h2 className="no-span">Images</h2>
+                        <div className='row'>
+                            <div className='column'>
+                                {arrayImages.map((data, index) => {
+                                    if (isEven(index) || index === 0) {
+                                        return <img className='images' key={index} onClick={() => {
+                                            handleImage(data)
+                                        }} alt='' src={data}/>
+                                    }
+                                })}
+                            </div>
+                            <div className='column'>
+                                {arrayImages.map((data, index) => {
+                                    if (isOdd(index)) {
+                                        return <img className='images' key={index} onClick={() => {
+                                            handleImage(data)
+                                        }} alt='' src={data}/>
+                                    }
+                                })}
+                            </div>
+                        </div>
+                        {portalOpen &&
+                        <Portal id='modal-root'>
+                            <BuildModal openPortal={openPortal} image={portalImage}/>
+                        </Portal>
+                        }
+                    </div>
                 </div>
             </div>
+            }
         </div>
     )
 
